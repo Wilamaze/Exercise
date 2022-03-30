0480697b9c73c1d85a0cabbc52be353c57e908cd
Orlando Aguilar <orlando.aguilar@live.com>
2022-03-30 17:45:46 -0400

    Added exercise.py

---
+++ exercise.py
@@ -0,0 +1,21 @@
+""" Python file for the exercise assignment"""\
+    
+class Person:
+    """A person within our group
+    
+    Attributes:
+    number (int): The person's number we are assigning them to.
+    name (str): The person's name within our group.
+    """
+    
+    def __init__(self, number, name):
+        self.number = number
+        self.name = name
+    
+    def __str__(self):
+        return "Group Member Number " + str(self.number) + \
+            " and their name is " + self.name + "!"
+    
+# Group Members putting their number and name here.
+orlando = Person(1, "Orlando Aguilar")
+print(orlando)
